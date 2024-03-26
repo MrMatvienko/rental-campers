6e1432923b340cf7db5cbe6f58e273448312638c
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-03-26 16:06:42 +0200

    rental link

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
-import CSS from "./Home.module.css";
+import { Link } from 'react-router-dom';
+import CSS from './Home.module.css';
 const Home = () => {
   return (
     <div className={CSS.mainPages}>
       <h1 className={CSS.title}>The largest selection of campers for travel</h1>
-      <button className={CSS.rentalButton}>Rental</button>
+      <Link to="/rental" className={CSS.rentalButton}>
+        Rental
+      </Link>
     </div>
   );
 };
