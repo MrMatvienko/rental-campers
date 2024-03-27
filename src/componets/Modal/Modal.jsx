159a3ff0bc549a079b045ca277d177889bca2286
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-03-27 20:31:09 +0200

    update project file

--- src/componets/Modal/Modal.jsx
+++ src/componets/Modal/Modal.jsx
@@ -1,20 +1,20 @@
-import CSS from "./Modal.module.css";
-import sprite from "../../images/sprite.svg";
-import { useEffect } from "react";
+import CSS from './Modal.module.css';
+import sprite from '../../images/sprite.svg';
+import { useEffect } from 'react';
 const Modal = ({ camper, onClose }) => {
   useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === "Escape") {
+    const handleKeyDown = event => {
+      if (event.key === 'Escape') {
         onClose();
       }
     };
-    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener('keydown', handleKeyDown);
     return () => {
-      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener('keydown', handleKeyDown);
     };
   }, [onClose]);
 
-  const handleBackdropClick = (event) => {
+  const handleBackdropClick = event => {
     if (event.target === event.currentTarget) {
       onClose();
     }
@@ -27,7 +27,7 @@ const Modal = ({ camper, onClose }) => {
         <div className={CSS.firstLine}>
           <h2 className={CSS.title}>{camper.name}</h2>
           <svg className={CSS.icon} onClick={onClose}>
-            <use href={sprite + "#icon-close"} />
+            <use href={sprite + '#icon-close'} />
           </svg>
         </div>
         <div className={CSS.secondLine}>
