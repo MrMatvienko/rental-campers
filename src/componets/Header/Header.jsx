3888098006ae35f4ffd56f7e1d8ec5e2a127f5ab
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-10-19 20:46:05 +0300

    media screen

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -3,27 +3,29 @@ import CSS from './Header.module.css';
 import sprite from '../../assets/images/sprite.svg';
 const Header = () => {
   return (
-    <div className={CSS.headerContainer}>
-      <svg className={CSS.iconList}>
-        <use href={sprite + '#icon-camp3'} />
-      </svg>
-      <ul className={CSS.navigation}>
-        <li className={CSS.headerItem}>
-          <NavLink className={CSS.navLink} to="/">
-            Home
-          </NavLink>
-        </li>
-        <li className={CSS.headerItem}>
-          <NavLink className={CSS.navLink} to="/rental">
-            Rental
-          </NavLink>
-        </li>
-        <li className={CSS.headerItem}>
-          <NavLink className={CSS.navLink} to="/favorites">
-            Favorites
-          </NavLink>
-        </li>
-      </ul>
+    <div className={CSS.container}>
+      <div className={CSS.headerContainer}>
+        <svg className={CSS.iconList}>
+          <use href={sprite + '#icon-camp3'} />
+        </svg>
+        <ul className={CSS.navigation}>
+          <li className={CSS.headerItem}>
+            <NavLink className={CSS.navLink} to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className={CSS.headerItem}>
+            <NavLink className={CSS.navLink} to="/rental">
+              Rental
+            </NavLink>
+          </li>
+          <li className={CSS.headerItem}>
+            <NavLink className={CSS.navLink} to="/favorites">
+              Favorites
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
