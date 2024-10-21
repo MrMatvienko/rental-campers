76c1056f5dc52c548e2680be029efd570d414e86
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-10-21 13:32:19 +0300

    homepage adaptive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,14 @@ import CSS from './Home.module.css';
 const Home = () => {
   return (
     <div className={CSS.mainPages}>
-      <h1 className={CSS.title}>The largest selection of campers for travel</h1>
-      <Link to="/rental" className={CSS.rentalButton}>
-        Rental
-      </Link>
+      <div className={CSS.containerHomePage}>
+        <h1 className={CSS.title}>
+          The largest selection of campers for travel
+        </h1>
+        <Link to="/rental" className={CSS.rentalButton}>
+          Rental
+        </Link>
+      </div>
     </div>
   );
 };
